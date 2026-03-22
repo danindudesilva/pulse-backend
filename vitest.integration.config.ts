d23f5ce3aa31a8c7bf02455b1f1bd8e5a48c5b83
Danindu de Silva <danindu001@gmail.com>
2026-03-22 18:42:51 +0000

    fix: clean test configs with env.test

---
+++ vitest.integration.config.ts
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv';
+import { defineConfig } from 'vitest/config';
+
+dotenv.config({ path: '.env.test' });
+
+export default defineConfig({
+  test: {
+    environment: 'node',
+    include: ['src/tests/integration/**/*.test.ts']
+  }
+});
