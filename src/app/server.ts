543b85f529fc2c1820e5323a2f91904909089101
Danindu de Silva <danindu001@gmail.com>
2026-03-22 16:52:51 +0000

    feat: update server to construct the opprtunity repository and service

--- src/app/server.ts
+++ src/app/server.ts
@@ -1,14 +1,22 @@
-import { createApp } from './app.js';
+import { env } from '../config/env.js';
 import { prisma } from '../lib/db/prisma.js';
+import { createApp } from './app.js';
 import { BootstrapUserService } from '../modules/identity/application/bootstrap-user.service.js';
 import { PrismaIdentityRepository } from '../modules/identity/infrastructure/prisma-identity.repository.js';
-import { env } from '../config/env.js';
+import { CreateOpportunityService } from '../modules/opportunities/application/create-opportunity.service.js';
+import { PrismaOpportunityRepository } from '../modules/opportunities/infrastructure/prisma-opportunity.repository.js';
 
 const identityRepository = new PrismaIdentityRepository(prisma);
 const bootstrapUserService = new BootstrapUserService(identityRepository);
 
+const opportunityRepository = new PrismaOpportunityRepository(prisma);
+const createOpportunityService = new CreateOpportunityService(
+  opportunityRepository
+);
+
 const app = createApp({
-  bootstrapUserService
+  bootstrapUserService,
+  createOpportunityService
 });
 
 app.listen(env.PORT, () => {
