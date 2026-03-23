98ddc49ff7b0b5ede04f3fd80e3dcb3146c2704f
Danindu de Silva <danindu001@gmail.com>
2026-03-23 19:29:53 +0000

    test: fix test db name for followups

--- docker/test/init-test-databases.sql
+++ docker/test/init-test-databases.sql
@@ -1,3 +1,3 @@
 CREATE DATABASE pulse_backend_test_identity;
 CREATE DATABASE pulse_backend_test_opportunities;
-CREATE DATABASE pulse_backend_test_followup;
+CREATE DATABASE pulse_backend_test_followups;
